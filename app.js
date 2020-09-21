0f22086bb36a859d7f71ac344d0e9db1e15de7d9
DIVYANKAR BHARGAV <divyankarbhargav@gmail.com>
2020-09-21 12:28:05 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express      = require('express'),
+    app          = express(),
+    bodyParser   = require('body-parser'),
+    path         = require('path');
+
+var port = process.env.PORT || 3000;
+
+// Requiring Routes
+var indexRouter = require('./routes/index');
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+// Set the default views directory to public folder
+app.use(express.static(path.join(__dirname, 'public/css')));
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+    console.log('Server is running at localhost: '+ port);
+});
